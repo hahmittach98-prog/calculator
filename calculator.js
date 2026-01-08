7539eda2ce623dc2110a5e8c7aef68367cb7a465
Hajar <hahmittach98@gmail.com>
2026-01-08 11:05:22 +0100

    modification du fichier calculator

--- calculator.js
+++ calculator.js
@@ -3,3 +3,5 @@ console.log("2 + 3 =", add(2, 3));
 console.log("5 - 2 =", subtract(5, 2));
 console.log("4 * 3 =", multiply(4, 3));
 console.log("10 / 2 =", divide(10, 2));
+
+console.log("Modification local - test push");
